5f0ae5cd7e54437fee370da935c825a3d44cbadd
Kavia AI <production@kavia.ai>
2025-07-17 14:36:35 +0000

    CheckPoint - cmfcff00ac

--- .init/.linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/kavia/workspace/code-generation/user-request-test-project-86711f5a/frontend_web_app
-npm run build
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-   exit 1
-fi
-
